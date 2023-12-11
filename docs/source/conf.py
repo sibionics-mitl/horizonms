5e9a5bb294fb0949586fd0feea50181595e3af17
xc.pan <xc.pan@sibionics.com>
2023-12-11 22:41:37 +0800

    conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,11 +7,12 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
+import os
+import sys
 
 # sys.path.insert(0, os.path.abspath('../examples'))
 # sys.path.insert(0, os.path.abspath('F:\anaconda2022\gitlab\hms_pub'))
+sys.path.insert(0, os.path.abspath('./../..'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
